f63504208a7d33b76588072d6f50acd33bd8f0ba
Nei Nei <satsuya0114@gmail.com>
2018-06-25 15:14:17 +0800

    trim part of words on txt

--- containers/Home/Home.js
+++ containers/Home/Home.js
@@ -5,7 +5,7 @@ export default class Home extends React.PureComponent {
   render() {
     return (
       <div style={{ textAlign: 'center' }} >
-        <h2 className="orange">Hello, This is webpack demo for React</h2>
+        <h2 className="orange">Hello, This is parcel demo for React</h2>
         <p>:D :O</p>
       </div>
     );
